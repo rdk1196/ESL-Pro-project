178b73fbbbfcca08f542c0330271b50ab37e25ac
Roman <romankurockin473@gmail.com>
2025-07-26 14:01:58 +0300

    Promo v1 Final — full responsive layout completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,31 @@
+import Swiper from 'swiper';
+import { Pagination, Navigation } from 'swiper/modules';
+
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import "/src/sass/style.scss";
 
+try {
+    new Swiper(".schedule__swiper", {
+        modules: [Pagination, Navigation],
+        loop: true, // Зациклить слайдер
+        slidesPerView: 4,
+        spaceBetween: 40,
+        speed: 1000,
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+
+        navigation: {
+            nextEl: ".schedule__next",
+            prevEl: ".schedule__prev",
+        }
+
+    });
+}
+catch (e) { }
 
